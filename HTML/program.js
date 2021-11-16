a9568b3f2843040a08906bc2be866b2302cf5211
kvelraj_concord <kvelraj@concord.net>
2021-11-16 10:57:28 +0530

    js

---
+++ HTML/program.js
@@ -0,0 +1,24 @@
+// Number -> Any value from Positive, Negative, Fractional, Decimal
+// var variable_name = value;
+// variable_name -> Any valid text, it should contains a-z, A-Z, 0-9, _, $
+
+var age = 27;
+console.log(age);
+
+var account_number = 872122552563;
+console.log(account_number);
+
+//String - Any value which is enclosed inside single and double quotes
+
+var user_name = "John Smith";
+console.log(user_name);
+
+//Boolean - Either true or false
+
+var result = true;
+console.log(result);
+
+//Undefined - The variable has been created but the values has not been defined
+
+var empty_data;
+console.log(empty_data);
