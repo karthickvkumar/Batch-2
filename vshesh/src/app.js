1ebcfaf1d1fdc7db48ff1e043f638819a6615958
kvelraj_concord <kvelraj@concord.net>
2021-11-25 09:56:27 +0530

    app js

---
+++ vshesh/src/app.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Welcome! React Application</h1>
+        <h1>This is a Main page</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
--- vshesh/src/index.js
+++ vshesh/src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./app";
 
-ReactDOM.render(<h1>Hello, Welcome to React</h1>, document.getElementById("root"));
+ReactDOM.render(<App></App>, document.getElementById("root"));
