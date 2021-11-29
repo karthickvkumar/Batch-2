8a044c7d99fdefcd0edc3d797917d4da4cf97aeb
kvelraj_concord <kvelraj@concord.net>
2021-11-29 10:37:06 +0530

    nesting routing

--- vshesh/src/app.js
+++ vshesh/src/app.js
@@ -6,6 +6,8 @@ import ContactPage from "./pages/contact";
 import HomePage from "./pages/home";
 import LoginPage from "./pages/login";
 import RegisterPage from "./pages/register";
+import LandingPage from "./pages/landing";
+import MailPage from "./pages/mail";
 
 import "./css/style.css";
 
@@ -15,11 +17,13 @@ class Application extends Component{
     return(
       <BrowserRouter>
         <Switch>
+          <Route path="/" exact component={LandingPage}></Route>
           <Route path="/about-us" component={AboutUsPage}></Route>
           <Route path="/contact" component={ContactPage}></Route>
           <Route path="/home" component={HomePage}></Route>
           <Route path="/login" component={LoginPage}></Route>
           <Route path="/signup" component={RegisterPage}></Route>
+          <Route path="/mail" component={MailPage}></Route>
         </Switch>
       </BrowserRouter>
     )
