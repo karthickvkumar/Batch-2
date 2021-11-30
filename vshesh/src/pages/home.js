884d634ad82a3da047134a6805446531d01ca6bf
kvelraj_concord <kvelraj@concord.net>
2021-11-30 10:49:00 +0530

    state

--- vshesh/src/pages/home.js
+++ vshesh/src/pages/home.js
@@ -2,6 +2,32 @@ import React, {Component} from "react";
 import {NavLink} from "react-router-dom";
 
 class HomePage extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      message : ''
+    }
+  }
+
+  checkVotingAge(){
+    var value = prompt("Enter your age :");
+    var age = parseInt(value);
+
+    if(age >= 18){
+      //console.log("This user is Eligible for Vote");
+      this.setState({
+        message : "This user is Eligible for Vote"
+      })
+    }
+    else{
+      //console.log("This user is NOT Eligible for Vote");
+      this.setState({
+        message : "This user is NOT Eligible for Vote"
+      })
+    }
+  }
+
   render(){
     return(
       <div>
@@ -9,6 +35,9 @@ class HomePage extends Component{
         <NavLink to="/login">Back to Login page</NavLink>
         <br></br>
         <NavLink to="/contact">Click here, to go Contact Page</NavLink>
+        <br></br>
+        <button onClick={() => this.checkVotingAge()}>Click to run Method</button>
+        <h1>{this.state.message}</h1>
       </div>
     )
   }
