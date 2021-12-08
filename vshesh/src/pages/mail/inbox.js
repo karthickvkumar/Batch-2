24e01a57c874b86258de6673dce8c7e6856c3b39
kvelraj_concord <kvelraj@concord.net>
2021-12-08 10:29:04 +0530

    get api

--- vshesh/src/pages/mail/inbox.js
+++ vshesh/src/pages/mail/inbox.js
@@ -1,10 +1,28 @@
 import React, {Component} from "react";
+import axios from "axios";
+
 
 class InboxPage extends Component{
+
+  loadUsers(){
+    var api_url = "https://reqres.in/api/users?page=2";
+
+    axios.get(api_url)
+        .then((response) => {
+            var serverData = response.data;
+            console.log(serverData.data)
+        })
+        .catch((error) => {
+            console.log(error);
+        }); 
+
+  }
+
   render(){
     return(
       <div>
         <h1>This is a Inbox Page</h1>
+        <button onClick={() => this.loadUsers()}>Load Users from API</button>
       </div>
     )
   }
